e4f8b3bf17fd89ceca94a6975a2f1e587ad30995
Mohit Saini <mohitsaini@Mohits-MacBook-Pro-2.local>
2017-11-20 16:16:35 +0530

    dependency added proj1

--- setup.py
+++ setup.py
@@ -10,7 +10,10 @@ setup(
 	description='proj1',
 	long_description='proj1',
 	packages=['proj1'],
-	install_requires=[],
+	install_requires=["msl==1.0.0"],
+	dependency_links = [
+		"https://github.com/mohitmv/msl3/tarball/master#egg=msl-1.0.0"
+	], 
 	extras_require={}, 
 	classifiers=[
 		'Programming Language :: Python :: 3',
